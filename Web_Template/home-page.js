78ba10b5e710aecea9b53e7c48b3ed8c30d7baee
Atharva Joshi <79744367+atharvajoshi1007@users.noreply.github.com>
2021-11-09 16:35:54 +0530

    Add files via upload

---
+++ Web_Template/home-page.js
@@ -0,0 +1,8 @@
+//function section in Javascript and Jquery...
+function myData() {
+    retrun;
+  }
+  
+  function show() {
+    document.getElementById('anotherFunction').classList.toggle('Active');
+  }  
